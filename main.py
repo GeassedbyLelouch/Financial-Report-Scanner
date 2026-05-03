77abc917d21f4c96fa65180f6eff7785b478b9f1
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-05-03 13:15:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import fitz
 
 pdf_path = r"C:\Users\TronM\Documents\AJ\Apple_2025_10-K.pdf"
 doc = fitz.open(pdf_path)
-for(int page_number = 0; page_number + 1 == len(doc); page_number++){
-  
-page = doc[page_number]
-text = page.get_text()
-
-print(text)
-}
+for page_number in range(len(doc)):
+  page = doc[page_number]
+  text = page.get_text()
+  print(text)
+  if page_number + 1 == len(doc):
+    break
