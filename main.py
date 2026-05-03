325cf4c5fc4d8368dd8467694f61bf01eb005f74
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-05-03 13:11:30 -0400

    Fix loop to print all pages in PDF document

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import fitz
 
 pdf_path = r"C:\Users\TronM\Documents\AJ\Apple_2025_10-K.pdf"
 doc = fitz.open(pdf_path)
-page = doc[0]
+for(int page_number = 0; page_number + 1 == len(doc); page_number++){
+  
+page = doc[page_number]
 text = page.get_text()
 
-print(text[:500])
+print(text)
+}
