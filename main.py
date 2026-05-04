eece05930f6d78c913603a9f52b7aac84ffdaaf5
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-05-04 11:50:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import fitz
 import streamlit as st
 
-pdf_path = "./data/Apple_2025_10-K.pdf"
-doc = fitz.open(pdf_path)
+uploaded_file = st.file_uploader(
+  label = "Upload a financial report",
+  type = ["pdf"],
+  accept_multiple_files =True,
+  help = "Please only upload pdf files"
+)
 for page_number in range(len(doc)):
   page = doc[page_number]
   text = page.get_text()
