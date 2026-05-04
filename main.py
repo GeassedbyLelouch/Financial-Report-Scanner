317107ccf42b80ce432dc1ffa680f1bd8c46f034
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-05-04 12:48:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ uploaded_file = st.file_uploader(
   type = ["pdf"],
   help = "Please only upload pdf files"
 )
-finished_file = fitz.open(stream=uploaded_file.read(), filetype=pdf)
+intermediate_file = stream=uploaded_file.read()
+finished_file = fitz.open(intermediate_file, filetype="pdf")
 
 for page_number in range(len(finished_file)):
   page = finished_file[page_number]
-  text = page.extract_text()
+  text = page.get_text()
   st.text_area("Extracted Text", text)
  
