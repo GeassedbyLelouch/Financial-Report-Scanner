65526e798adba55b7306ef39a6b6eff88ad24ca9
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-05-04 12:08:19 -0400

    Remove support for multiple file uploads

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import streamlit as st
 uploaded_file = st.file_uploader(
   label = "Upload a financial report",
   type = ["pdf"],
-  accept_multiple_files =True,
   help = "Please only upload pdf files"
 )
 for page_number in range(len(uploaded_file)):
