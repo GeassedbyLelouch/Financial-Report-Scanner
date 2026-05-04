ab5890e10278115777f441cff4bf52371c5936ba
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-05-04 10:29:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import fitz
 
-pdf_path = r"C:\Users\TronM\Documents\AJ\Apple_2025_10-K.pdf"
+pdf_path = "./data/Apple_2025_10-K.pdf"
 doc = fitz.open(pdf_path)
 for page_number in range(len(doc)):
   page = doc[page_number]
   text = page.get_text()
   print(text)
-  if page_number + 1 == len(doc):
-    break
+ 
