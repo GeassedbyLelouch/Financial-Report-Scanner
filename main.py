4349fd464b0ffcbefee3a722e207ee18dfd9cd78
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-05-07 12:59:43 -0400

    Fix variable name and improve text extraction

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ uploaded_file = st.file_uploader(
 )
 total = ""
 if uploaded_file is not None:
-  bytes = uploaded_file.read()
+  file_bytes = uploaded_file.read()
   finished_file = fitz.open(stream=bytes, filetype="pdf")
   for page_number in range(len(finished_file)):
     page = finished_file[page_number]
     text = page.get_text()
-    for line in text:
+    for line in text.split('\n'):
       if(line.strip().isdigit()):
         total+=line  
 
