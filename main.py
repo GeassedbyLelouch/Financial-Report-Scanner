2432990ae48f787629b394d1457bb84d30d43c60
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-06-15 19:58:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ if uploaded_file is not None:
               if(word.translate(str.maketrans(replacements)).isdigit()):
                 final_word = word.translate(str.maketrans(replacements))
                 total = int(final_word)
-                print(line"\n")
-                print(total"\n")
-                print(final_word"\n")
+                print(f"{line}\n")
+                print(f"{total}\n")
+                print(f"{final_word}\n")
                 Free_Cash_Flow -= total
                 break;
               else:
